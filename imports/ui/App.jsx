8a9148674f75ded45f0c741747b886676c10c47b
codeconcept <contact@codeconcept.fr>
2019-10-26 13:21:58 +0200

    Creation form for new poll

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "./Form";
+
+const App = () => (
+  <div>
+    <h1>AC Voting</h1>
+    <Form />
+  </div>
+);
+
+export default App;
---
+++ imports/ui/Form.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+class Form extends React.Component {
+  state = {
+    question: "",
+    answer1: { text: "", votes: 0 },
+    answer2: { text: "", votes: 0 },
+    answer3: { text: "", votes: 0 }
+  };
+  handleChange = (evt, id) => {
+    if (id === "question") {
+      this.setState({
+        question: evt.target.value
+      });
+    } else {
+      this.setState({
+        [id]: { ...this.state[id], text: evt.target.value }
+      });
+    }
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log("submit");
+  };
+  render() {
+    return (
+      <div>
+        <h3>Créer un nouveau sondage</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="question"
+            placeholder="question"
+            value={this.state.question}
+            onChange={evt => this.handleChange(evt, "question")}
+          />
+          <br />
+          <input
+            type="text"
+            name="option1"
+            placeholder="option 1"
+            onChange={evt => this.handleChange(evt, "answer1")}
+            onBlur={evt => this.handleChange(evt, "answer1")}
+          />
+          <br />
+          <input
+            type="text"
+            name="option2"
+            placeholder="option 2"
+            onChange={evt => this.handleChange(evt, "answer2")}
+            onBlur={evt => this.handleChange(evt, "answer2")}
+          />
+          <br />
+          <input
+            type="text"
+            name="option3"
+            placeholder="option 3"
+            onChange={evt => this.handleChange(evt, "answer3")}
+            onBlur={evt => this.handleChange(evt, "answer3")}
+          />
+          <br />
+          <button type="submit">créer</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
